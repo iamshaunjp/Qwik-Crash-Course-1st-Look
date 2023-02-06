13f458fd49dbf636e5c75fa2e17a950727ccb21c
Shaun <shaun@thenetninja.co.uk>
2023-02-06 17:25:26 +0000

    lesson-11

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,32 +1,44 @@
-import { component$, useSignal, useStore } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { component$, Resource } from '@builder.io/qwik';
+import type { DocumentHead, RequestHandler } from '@builder.io/qwik-city';
+import { useEndpoint } from '@builder.io/qwik-city';
+
+interface BlogData {
+  id: string,
+  title: string,
+  content: string
+}
+
+export const onGet: RequestHandler<BlogData[]> = async () => {
+  console.log('fetching the data') // always on the server
+
+  const res = await fetch('http://localhost:3000/blogs')
+  const data = await res.json()
+
+  return data
+}
 
 export default component$(() => {
-  const name = useSignal('mario')
-
-  const person = useStore({ name: 'peach', age: 30,  })
-
-  const blogs = useStore([
-    {id: 1, title: 'my first blog'},
-    {id: 2, title: 'my second blog'},
-    {id: 3, title: 'marmite rules!'}
-  ])
+  const blogsData = useEndpoint<BlogData[]>()
 
   return (
     <div>
       <h1>Okie Dokie!</h1>
 
-      <p>Hello, {name.value}</p>
-      <p>Hello, {person.name}, you are {person.age} years young</p>
+      <Resource 
+        value={blogsData}
+        onPending={() => <div>Loading blogs...</div>}
+        onResolved={(blogs) => (
+          <div class="blogs">
+            {blogs && blogs.map(blog => (
+              <div key={blog.id}>
+                <h3>{blog.title}</h3>
+                <p>{blog.content.slice(0,50)}...</p>
+              </div>
+            ))}
+          </div>
+        )}
+      />
 
-      <button onClick$={() => name.value = 'luigi'}>click me</button>
-      <button onClick$={() => person.name = 'bowser'}>click me again</button>
-
-      {blogs.map(blog => (
-        <div key={blog.id}>{blog.title}</div>
-      ))}
-
-      <button onClick$={() => blogs.pop()}>remove a blog</button>
     </div>
   );
 });

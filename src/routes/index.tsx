ff815abc5c36c919b71f1f83d75c8874fec2b282
Shaun <shaun@thenetninja.co.uk>
2023-02-06 10:46:35 +0000

    lesson-6

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,32 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useSignal, useStore } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
+  const name = useSignal('mario')
+
+  const person = useStore({ name: 'peach', age: 30,  })
+
+  const blogs = useStore([
+    {id: 1, title: 'my first blog'},
+    {id: 2, title: 'my second blog'},
+    {id: 3, title: 'marmite rules!'}
+  ])
+
   return (
     <div>
       <h1>Okie Dokie!</h1>
+
+      <p>Hello, {name.value}</p>
+      <p>Hello, {person.name}, you are {person.age} years young</p>
+
+      <button onClick$={() => name.value = 'luigi'}>click me</button>
+      <button onClick$={() => person.name = 'bowser'}>click me again</button>
+
+      {blogs.map(blog => (
+        <div key={blog.id}>{blog.title}</div>
+      ))}
+
+      <button onClick$={() => blogs.pop()}>remove a blog</button>
     </div>
   );
 });

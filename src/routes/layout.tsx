d1826708dae3e375035859e11000a14e09027920
Shaun <shaun@thenetninja.co.uk>
2023-02-06 18:08:52 +0000

    lesson-15

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,7 +1,41 @@
-import { component$, Slot } from '@builder.io/qwik';
+import { component$, Slot, useClientEffect$, useSignal, useTask$ } from '@builder.io/qwik';
 import Header from '../components/header/header';
 
 export default component$(() => {
+  const counter = useSignal(0)
+  const tens = useSignal(0)
+
+  // runs on the server
+  // then after every change to tracked state
+  // use if you want to ren code pre-render on the server
+  // use if you want to run code in the browser after state changes
+  useTask$(({ track }) => {
+    // console.log('useTask -', counter.value)
+    track(counter)
+
+    if (counter.value % 2 === 0) {
+      console.log('useTask -', counter.value)
+    }
+    if (counter.value === 10) {
+      tens.value++
+    }
+  })
+
+  // runs on the client only, eagerly
+  // also after every change to tracked state
+  // use if you need to immediately run code in the browser
+  useClientEffect$(({ track }) => {
+    track(tens)
+
+    console.log('useClientEffect', tens.value)
+    counter.value = 0;
+    
+    const timer = setInterval(() => counter.value++, 1000)
+
+    // cleanup runs each time function gets fired after first time
+    return () => clearInterval(timer)
+  })
+
   return (
     <>
       <main>
